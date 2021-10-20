bd27a26cbf3a22d104ddae74ff3daeade32ef15e
GitHub Action <action@github.com>
2021-10-20 03:08:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/tonylituo/tonyplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tonylituo/tonyplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tonylituo.tonyplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tonylituo.tonyplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tonylituo/tonyplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tonylituo/tonyplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tonylituo.tonyplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tonylituo.tonyplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
